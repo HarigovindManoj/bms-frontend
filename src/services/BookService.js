04af1bdc3db2ab7d56b79145c1d83d521b94cfe7
HarigovindManoj <harigovindmanoj1@gmail.com>
2025-03-11 20:01:19 +0530

    service commit

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -5,33 +5,33 @@ const API_URL = 'https://book-management-backend-2-66di.onrender.com';
 export const createBook = async (bookData) => {
     // Check if bookData is FormData (has image) or regular JSON
     if (bookData instanceof FormData) {
-      return axios.post(`${API_URL}/createBook`, bookData, {
+      return axios.post(`${API_URL}/book/createBook`, bookData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       }).then(response => response.data);
     } else {
       // Regular JSON submission (no image)
-      return axios.post(`${API_URL}/createBookJson`, bookData)
+      return axios.post(`${API_URL}/book/createBookJson`, bookData)
         .then(response => response.data);
     }
 };
 
 export const getAllBooks = async() => {
-    const response = await axios.get(`${API_URL}/allBooks`);
+    const response = await axios.get(`${API_URL}/book/allBooks`);
     return response.data;
 }
 
 export const getBookById = async (id) => {
-    const response = await axios.get(`${API_URL}/getbook/${id}`);
+    const response = await axios.get(`${API_URL}/book/getbook/${id}`);
     return response.data;
 }
 
 export const deleteBook = async (id) => {
-    await axios.delete(`${API_URL}/delete/${id}`);
+    await axios.delete(`${API_URL}/book/delete/${id}`);
 }
 
 export const getExternalBookDetails = async (isbn) => {
-    const response = await axios.get(`${API_URL}/external/${isbn}`);
+    const response = await axios.get(`${API_URL}/book/external/${isbn}`);
     return response.data;
   };
